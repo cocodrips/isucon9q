9c962b833aa41af5af0d9427c04cac59ff8d8513
cocodrips <cocodrips@gmail.com>
2019-09-08 13:57:23 +0900

    Create flatten category.

---
+++ webapp/sql/10_create_flatten_category.sql
@@ -0,0 +1,13 @@
+CREATE TABLE category_flatten AS
+(SELECT
+  t3.id                    AS id,
+  categories.id            AS parent_id,
+  categories.category_name AS parent_name,
+  t3.category_child        AS name
+FROM (SELECT
+        id,
+        parent_id,
+        category_name AS category_child
+      FROM categories) AS t3 LEFT JOIN categories ON t3.parent_id = categories.id);
+
+ALTER TABLE category_flatten ADD PRIMARY KEY(id);
